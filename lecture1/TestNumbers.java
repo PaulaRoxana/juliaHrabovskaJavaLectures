2a6cc1f7e69210da1526e73446b9606143dd717e
Paula Girdea <paula_girdea@epam.com>
2022-08-09 22:12:23 +0300

    08.09 commit

--- lecture1/TestNumbers.java
+++ lecture1/TestNumbers.java
@@ -1,13 +1,48 @@
+import java.util.Scanner;
+
 public class TestNumbers {
-		public static void main(String[] args) {
-		int a = 3;
-		int b = 5;
-		int result = a + b;
-		System.out.println("Sum of 3 and 5 = " + result);  
-		
-		double c = 8.5;
-		double d = 5.3;
-		double result1 = c - d;
-		System.out.println("Result of subtraction of 8.5 and 5.3 = " + result1);
-	}
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int result = a + b;
+        System.out.println("Sum of 3 and 5 = " + result);
+
+        double c = 8.5;
+        double d = 5.3;
+        double result1 = c - d;
+        System.out.println("Result of subtraction of 8.5 and 5.3 = " + result1);
+
+        System.out.println(electronicWatch(86400));
+        System.out.println(electronicWatch(86399));
+
+        System.out.println(electronicWatch(70));
+
+
+
+
+// OOP_HT1_practiceFor
+     //   Scanner scanner = new Scanner(System.in);
+      //  String name = scanner.nextLine();
+    //    System.out.println("Hello" + " " + name);
+
+
+    }
+
+    public static String electronicWatch (int seconds) {
+
+        int secondsPerDay = 86400;
+        int secondsPerHour = 3600;
+        int secondsPerMinute = 60;
+
+        int remainingsOf1Day = seconds % secondsPerDay;
+        int remainingsOf1Hr = remainingsOf1Day % 3600;
+        int h = remainingsOf1Day / 3600;
+        int m = remainingsOf1Hr / 60;
+        int s = remainingsOf1Hr % 60;
+
+
+        return (m<10 && s<10)? (h + ":0" + m + ":0" + s) : (h + ":" + m + ":" + s);
+
+    }
+
 }
