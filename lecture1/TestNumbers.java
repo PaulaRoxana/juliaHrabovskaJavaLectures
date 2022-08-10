a91f260f00d5857a709e789aaf819c52f47e1f52
Paula Girdea <paula_girdea@epam.com>
2022-08-10 08:05:11 +0300

    08.10 commit.2

--- lecture1/TestNumbers.java
+++ lecture1/TestNumbers.java
@@ -25,10 +25,28 @@ public class TestNumbers {
       //  String name = scanner.nextLine();
     //    System.out.println("Hello" + " " + name);
 
+        Scanner scanner = new Scanner(System.in);
+        int seconds = scanner.nextInt();
+
+        int secondsPerDay = 86400;
+        int secondsPerHour = 3600;
+        int secondsPerMinute = 60;
+
+        int remainingsOf1Day = seconds % secondsPerDay;
+        int remainingsOf1Hr = remainingsOf1Day % 3600;
+        int h = remainingsOf1Day / 3600;
+        int m = remainingsOf1Hr / 60;
+        int s = remainingsOf1Hr % 60;
+
+        System.out.println( String.format("%02d", h) + ":" + String.format("%02d", m) + ":" + String.format("%02d", s));
+
+
+
 
     }
 
     public static String electronicWatch (int seconds) {
+
 
         int secondsPerDay = 86400;
         int secondsPerHour = 3600;
